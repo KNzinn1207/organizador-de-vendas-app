e9f4465619d82f3cef19f0df72f5de22a8f8d846
KNzinn1207 <kenonbernardokenmoti@gmail.com>
2026-09-17 19:00:38 -0300

    Aumenta limite da coluna senha para 255 caracteres

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ class User(UserMixin, db.Model):
   __tablename__ = 'user'
   id = db.Column(db.Integer, primary_key=True)
   email = db.Column(db.String(150), unique=True, nullable=False)
-  senha = db.Column(db.String(150), nullable=False)
+  senha = db.Column(db.String(255), nullable=False)
   ativo = db.Column(
       db.Boolean, default=False
   )  # False = Aguardando aprovação, True = Aprovado
